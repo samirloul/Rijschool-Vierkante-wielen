f353517acad5be8e1956e1fdad86827434dd7113
samirloul <sameerloul2010@gmail.com>
2026-06-01 11:39:08 +0200

    web

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+Route::view('/over-ons', 'pages.about')->name('about');
+Route::view('/lespakketten', 'pages.packages')->name('packages');
+Route::view('/contact', 'pages.contact')->name('contact');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
