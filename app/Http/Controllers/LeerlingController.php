3867ccbc8493a0211ae22e5f87e8053ba1937263
nearrindell <nathaniaarrindell3@gmail.com>
2026-06-04 01:13:11 +0200

    Try and catch toegepast bij allebei de controllers

--- app/Http/Controllers/LeerlingController.php
+++ app/Http/Controllers/LeerlingController.php
@@ -4,26 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Exception;
 
 class LeerlingController extends Controller
 {
     public function index()
     {
-        // Roep de stored procedure aan om leerlingen op te halen
-        $leerlingen = collect(DB::select('CALL sp_get_leerlingen()'));
+        try {
+            // Roep de stored procedure aan om leerlingen op te halen
+            $leerlingen = collect(DB::select('CALL sp_get_leerlingen()'));
 
-        // Log het aantal geladen leerlingen
-        $count = $leerlingen->count();
-        if ($count > 0) {
-            Log::info("[Leerlingen] {$count} leerlingen succesvol geladen");
-        } else {
-            Log::warning('[Leerlingen] Geen leerlingen gevonden');
-        }
+            // Log het aantal geladen leerlingen
+            $count = $leerlingen->count();
+            if ($count > 0) {
+                Log::info("[Leerlingen] {$count} leerlingen succesvol geladen");
+            } else {
+                Log::warning('[Leerlingen] Geen leerlingen gevonden');
+            }
 
-        // Zet error bericht als er geen leerlingen zijn
-        $error = null;
-        if ($leerlingen->isEmpty()) {
-            $error = 'Geen leerlingen gevonden';
+            // Zet error bericht als er geen leerlingen zijn
+            $error = null;
+            if ($leerlingen->isEmpty()) {
+                $error = 'Geen leerlingen gevonden';
+            }
+        } catch (Exception $e) {
+            // Log de error
+            Log::error('[Leerlingen] Fout bij laden van leerlingen: ' . $e->getMessage());
+            
+            // Zet lege collectie en error bericht
+            $leerlingen = collect();
+            $error = 'Er is een fout opgetreden bij het laden van leerlingen';
         }
 
         return view('Leerling.index', compact('leerlingen', 'error'));
