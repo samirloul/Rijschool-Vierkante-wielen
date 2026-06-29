f0d7a6bf5df076dd8048cb039cf7ecf5cb36fc1f
Ahmad-Abbas-1 <abbs.os.ahmad@gmail.com>
2026-06-29 09:17:12 +0200

    comments

--- resources/views/rijlessen/index.blade.php
+++ resources/views/rijlessen/index.blade.php
@@ -5,7 +5,7 @@
     active="rijlessen"
 >
     <div class="flex items-center justify-between">
-        <div>{{-- Ruimte voor eventuele filters later --}}</div>
+        <div></div>
         <a href="{{ route('rijlessen.create') }}"
            class="inline-flex items-center gap-2 rounded-xl bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow transition hover:bg-blue-500 active:scale-95">
             <svg class="h-4 w-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
